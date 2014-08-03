99c10397a1e846eb6da2cc5a69152622596dbece
Saleh Albatati <0xack13@gmail.com>
2014-08-03 05:21:23 +0300

    Using lock condition

--- src/Reenlock.java
+++ src/Reenlock.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Reenlock {
 	
 	private int count = 0;
+	private Lock lock = new ReentrantLock();
+	private Condition cond = lock.newCondition();
 	
 	private void increment() {
 		for(int i=0; i<1000; i++) {
@@ -9,14 +16,38 @@ public class Reenlock {
 		}
 	}
 	public void Thread1() throws InterruptedException {
-		increment();
+		lock.lock();
+		
+		System.out.println("You gotta wait now!");
+		cond.await();
+		
+		System.out.println("Come on! You woke me up!");
+		
+		try {
+			increment();
+		} finally {
+			lock.unlock();
+		}
 	}
 	
 	public void Thread2() throws InterruptedException {
-		increment();
+		Thread.sleep(1000);
+		lock.lock();
+		
+		System.out.println("Press the return key");
+		new Scanner(System.in).nextLine(); 
+		System.out.println("Return key was just clicked!");
+		
+		cond.signal();
+		
+		try {
+			increment();
+		} finally {
+			lock.unlock();
+		}
 	}
 	
 	public void finished() {
-		System.out.print("Count is: " + count);
+		System.out.println("Count is: " + count);
 	}
 }
