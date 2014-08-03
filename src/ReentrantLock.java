aedc2e8831ff45b6276179f6044313a8189a3722
Saleh Albatati <0xack13@gmail.com>
2014-08-03 04:42:53 +0300

    Initial commit

---
+++ src/ReentrantLock.java
@@ -0,0 +1,58 @@
+
+public class Reenlock {
+	
+	private int count = 0;
+	
+	private void increment() {
+		for(int i=0; i<1000; i++) {
+			
+		}
+	}
+	public void Thread1() throws InterruptedException {
+		
+	}
+	
+	public void Thread2() throws InterruptedException {
+			
+		}
+	
+	public void finished() {
+		
+	}
+}
+
+public class ReentrantLock {
+
+	public static void main(String[] args) {
+		final Reenlock reenlock = new Reenlock();
+		
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				try {
+					reenlock.Thread1();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				try {
+					reenlock.Thread2();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+		reenlock.finished();
+	}
+
+}
